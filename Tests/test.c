bfc55b95346094fa451d258c5472b0a1b4862a90
Axel <ax.ferreol@gmail.com>
2023-08-03 11:53:33 +0200

    Clean repo

--- Tests/test.c
+++
@@ -1,6 +0,0 @@
-__attribute__ ((aligned (16))) char stack0[4096];
-
-void start(){
-    *((unsigned char *) 0x10000000UL) = 0x61;    
-    return;
-}
--- alloc.h
+++
@@ -1,4 +0,0 @@
-void allocinit();
-void free(void*);
-void* alloc();
-void memset(void*,char);
